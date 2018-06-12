6c77fa62070eb3ac414ac9122c10c3919b7c3ee7
dan-kwiat <dan.kwiat@gmail.com>
2018-06-12 17:22:59 +0100

    Add logarithmic number range modal for income filter

--- src/components/charities/filters/AddFilter.js
+++ src/components/charities/filters/AddFilter.js
@@ -3,18 +3,20 @@ import PropTypes from 'prop-types'
 import { Icon, Dropdown, Menu, Button } from 'antd'
 import { areasOfOperation, causes, beneficiaries, operations } from '../../../lib/filterValues'
 import { CheckboxModal } from './CheckboxModal'
+import { NumberRangeModal } from './NumberRangeModal'
 
 class AddFilter extends Component {
   state = {
     modalConfig: {
       type: null,
       name: null,
+      fieldName: null,
       options: [],
     },
   }
   menuItems = () => ([
     { key: '1', icon: 'environment-o', label: 'Location', modalConfig: { type: 'checkbox', options: ['one', 'two', 'three'] }},
-    { key: '6', icon: 'bank', label: 'Income', modalConfig: { type: 'checkbox', options: ['one', 'two', 'three'] }},
+    { key: '6', icon: 'bank', label: 'Income', modalConfig: { type: 'numberRange', name: 'Income Filter', fieldName: 'incomeRange' }},
     { key: '2', icon: 'global', label: 'Regions', modalConfig: { type: 'checkbox', name: 'Regions of Operation Filter', fieldName: 'areasOfOperation.id', options: areasOfOperation.map(x => ({ ...x, isChecked: false })) }},
     { key: '3', icon: 'medicine-box', label: 'Causes', modalConfig: { type: 'checkbox', name: 'Causes Filter', fieldName: 'causes.id', options: causes.map(x => ({ ...x, isChecked: false })) }},
     { key: '4', icon: 'team', label: 'Beneficiaries', modalConfig: { type: 'checkbox', name: 'Beneficiaries Filter', fieldName: 'beneficiaries.id', options: beneficiaries.map(x => ({ ...x, isChecked: false })) }},
@@ -33,6 +35,17 @@ class AddFilter extends Component {
       ))}
     </Menu>
   )
+  onCheckboxOk = options => {
+    const checkedIds = options.map(x => x.id)
+    const filterString = `${this.state.modalConfig.fieldName}=${checkedIds.join(',')}`
+    this.setState({ modalConfig: {} })
+    this.props.onNewFilter(filterString)
+  }
+  onNumberRangeOk = valueRange => {
+    const filterString = `${this.state.modalConfig.fieldName}=${valueRange.join(',')}`
+    this.setState({ modalConfig: {} })
+    this.props.onNewFilter(filterString)
+  }
   render() {
     return (
       <div>
@@ -40,13 +53,17 @@ class AddFilter extends Component {
           <Button icon='plus' placement='bottomCenter'>Add Filter</Button>
         </Dropdown>
         <CheckboxModal
-          isOpen={typeof this.state.modalConfig.name === 'string'}
-          modalConfig={this.state.modalConfig}
+          isOpen={this.state.modalConfig.type === 'checkbox'}
           onCancel={() => this.setState({ modalConfig: {} })}
-          onOk={filterString => {
-            this.setState({ modalConfig: {} })
-            this.props.onNewFilter(filterString)
-          }}
+          onOk={this.onCheckboxOk}
+          name={this.state.modalConfig.name}
+          options={this.state.modalConfig.options}
+        />
+        <NumberRangeModal
+          isOpen={this.state.modalConfig.type === 'numberRange'}
+          onCancel={() => this.setState({ modalConfig: {} })}
+          onOk={this.onNumberRangeOk}
+          name={this.state.modalConfig.name}
         />
       </div>
     )
