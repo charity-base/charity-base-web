d9300f2bc969bbb41c34716e7b7d6bd430451eb0
dan-kwiat <dan.kwiat@gmail.com>
2018-06-14 09:57:43 +0100

    Use real employee & volunteer numbers from updated API

--- src/components/charity/CharityPeople.js
+++ src/components/charity/CharityPeople.js
@@ -4,28 +4,36 @@ import { SectionOverview } from './SectionOverview'
 import { NoneText } from '../general/NoneText'
 import { DividerTitle } from '../general/DividerTitle'
 
-const CharityPeopleOverview = ({ trustees, onClick }) => (
+const CharityPeopleOverview = ({ trustees, employees, volunteers, onClick }) => (
   <SectionOverview title="People" onClick={onClick} hoverable>
-    <p>{trustees.names.length} Trustees {trustees.incorporated ? '(included incorporated entity)' : ''}</p>
-    <p>45 (for example) employees</p>
-    <p>12 (for example) volunteers</p>
+    <p>{trustees} Trustees</p>
+    <p>{employees} Employees</p>
+    <p>{volunteers} Volunteers</p>
   </SectionOverview>
 )
 CharityPeopleOverview.propTypes = {
-  trustees: PropTypes.object,
+  trustees: PropTypes.number,
+  employees: PropTypes.number,
+  volunteers: PropTypes.number,
   onClick: PropTypes.func,
 }
 
 
-const CharityPeople = ({ trustees }) => (
+const CharityPeople = ({ trustees, employees, volunteers, incorporated, names }) => (
   <div>
-    <DividerTitle title={`${trustees.names.length} Trustee${trustees.names.length > 1 ? 's' : ''}`} />
-    {trustees.incorporated && <p><NoneText>(this list includes an incorporated entity)</NoneText></p>}
-    {trustees.names.map((x, i) => <p key={i}>{x}</p>)}
+    {volunteers > 0 && <DividerTitle title={`${volunteers} Volunteer${volunteers > 1 ? 's' : ''}`} />}
+    {employees > 0 && <DividerTitle title={`${employees} Employee${employees > 1 ? 's' : ''}`} />}
+    <DividerTitle title={`${trustees} Trustee${trustees > 1 ? 's' : ''}`} />
+    {incorporated && <p><NoneText>(this list includes an incorporated entity)</NoneText></p>}
+    {names.map((x, i) => <p key={i}>{x}</p>)}
   </div>
 )
 CharityPeople.propTypes = {
-  trustees: PropTypes.object,
+  trustees: PropTypes.number,
+  employees: PropTypes.number,
+  volunteers: PropTypes.number,
+  incorporated: PropTypes.bool,
+  names: PropTypes.array,
 }
 
 export { CharityPeople, CharityPeopleOverview }
