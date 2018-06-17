e07b4fca1c14fd0d5b5f966791c4f9e97ae737fd
dan-kwiat <dan.kwiat@gmail.com>
2018-06-17 23:36:16 +0100

    List recent income & expenditure

--- src/components/charity/CharityFinancial.js
+++ src/components/charity/CharityFinancial.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import numeral from 'numeral'
+import { Row, Col } from 'antd'
 import { SectionOverview } from './SectionOverview'
+import { DividerTitle } from '../general/DividerTitle'
+
+const formatMoney = x => (
+  numeral(x).format('($ 0.0 a)').replace('$', '£')
+)
+
+const formatDate = x => (
+  new Date(Date.parse(x)).toDateString().substr(4)
+)
 
 const CharityFinancialOverview = ({ income, fyend, onClick }) => (
   <SectionOverview title="Financial" onClick={onClick} hoverable>
-    <p>Latest Income: {numeral(income.latest.total).format('($ 0.0 a)').replace('$', '£')}</p>
-    <p>Financial Year End: {fyend}</p>
+    <p>Latest Income: {formatMoney(income.latest.total)}</p>
   </SectionOverview>
 )
 CharityFinancialOverview.propTypes = {
@@ -18,8 +27,19 @@ CharityFinancialOverview.propTypes = {
 
 const CharityFinancial = ({ income, fyend }) => (
   <div>
-    <div>Latest Income: {numeral(income.latest.total).format('($ 0.0 a)').replace('$', '£')}</div>
-    <div>Financial Year End: {fyend}</div>
+    {income.annual.sort((a, b) => Date.parse(b.financialYear.end) - Date.parse(a.financialYear.end)).map((x, i) => (
+      <div key={i}>
+        <DividerTitle title={formatDate(x.financialYear.end)} />
+        <Row align='center' type='flex' gutter={16}>
+          <Col span={12} style={{textAlign: 'right'}}>Income:</Col>
+          <Col span={12} style={{textAlign: 'left'}}>{formatMoney(x.income)}</Col>
+        </Row>
+        <Row align='center' type='flex' gutter={16}>
+          <Col span={12} style={{textAlign: 'right'}}>Expenditure:</Col>
+          <Col span={12} style={{textAlign: 'left'}}>{formatMoney(x.expend)}</Col>
+        </Row>
+      </div>
+    ))}
   </div>
 )
 CharityFinancial.propTypes = {
