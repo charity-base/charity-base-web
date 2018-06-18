888ba1920a4f84d0ea844469109a4fd5f15fab71
dan-kwiat <dan.kwiat@gmail.com>
2018-06-18 18:22:36 +0100

    Add links to navbar

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,4 +1,4 @@
-const apiEndpoint = 'http://localhost:4000/api/v2.0.0'
-// const apiEndpoint = 'https://localhost/api/v2.0.0'
+// const apiEndpoint = 'http://localhost:4000/api/v2.0.0'
+const apiEndpoint = 'https://charitybase.uk/api/v2.0.0'
 
 export { apiEndpoint }
