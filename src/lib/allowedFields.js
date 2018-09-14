f1f9c5548dcedcedbf837ee6f12f5d0d227b2808
dan-kwiat <dan.kwiat@gmail.com>
2018-09-14 14:25:18 +0100

    Add to allowed fields for download

--- src/lib/allowedFields.js
+++ src/lib/allowedFields.js
@@ -3,45 +3,81 @@ const defaultFieldsList = [
   'name',
 ]
 
-const allowedFields = [{
-  title: 'IDs',
-  key: 'ids',
-  children: [{
-    title: 'CC Number',
-    key: 'ids.GB-CHC',
+const allowedFields = [
+  {
+    title: 'IDs',
+    key: 'ids',
+    children: [{
+      title: 'CC Number',
+      key: 'ids.GB-CHC',
+      disabled: true,
+    }, {
+      title: 'General ID',
+      key: 'ids.charityId',
+      disabled: false,
+    }],
     disabled: true,
   }, {
-    title: 'General ID',
-    key: 'ids.charityId',
-    disabled: false,
-  }],
-  disabled: true,
-}, {
-  title: 'Name',
-  key: 'name',
-  disabled: true,
-}, {
-  title: 'Contact',
-  key: 'contact',
-  children: [{
-    title: 'Email',
-    key: 'contact.email',
+    title: 'Name',
+    key: 'name',
+    disabled: true,
   }, {
-    title: 'Person',
-    key: 'contact.person',
+    title: 'Contact',
+    key: 'contact',
+    children: [{
+      title: 'Email',
+      key: 'contact.email',
+    }, {
+      title: 'Person',
+      key: 'contact.person',
+    }, {
+      title: 'Postcode',
+      key: 'contact.postcode',
+    }, {
+      title: 'Address',
+      key: 'contact.address',
+    }, {
+      title: 'Longitude',
+      key: 'contact.geo.longitude',
+    }, {
+      title: 'Latitude',
+      key: 'contact.geo.latitude',
+    }],
   }, {
-    title: 'Postcode',
-    key: 'contact.postcode',
+    title: 'People',
+    key: 'people',
+    children: [{
+      title: 'Number Volunteers',
+      key: 'people.volunteers',
+    }, {
+      title: 'Number Employees',
+      key: 'people.employees',
+    }, {
+      title: 'Number Trustees',
+      key: 'people.trustees',
+    }]
   }, {
-    title: 'Address',
-    key: 'contact.address',
+    title: 'Activities',
+    key: 'activities',
   }, {
-    title: 'Longitude',
-    key: 'contact.geo.longitude',
+    title: 'Website',
+    key: 'website',
   }, {
-    title: 'Latitude',
-    key: 'contact.geo.latitude',
-  }],
-}]
+    title: 'Latest Income',
+    key: 'income.latest.total',
+  }, {
+    title: 'Area of Benefit',
+    key: 'areaOfBenefit',
+  }, {
+    title: 'Causes',
+    key: 'causes',
+  }, {
+    title: 'Beneficiaries',
+    key: 'beneficiaries',
+  }, {
+    title: 'Operations',
+    key: 'operations',
+  },
+]
 
 export { defaultFieldsList, allowedFields }
