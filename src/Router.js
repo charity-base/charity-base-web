19a988f7f2736bd2fe57525c0cebf07800519298
dan-kwiat <dan.kwiat@gmail.com>
2019-02-12 14:47:55 +0000

    feat(analysis): temporarily remove analysis route

--- src/Router.js
+++ src/Router.js
@@ -22,9 +22,6 @@ const Router = ({ isMobile }) => (
     <Route exact path="/api-portal" render={() => (
       <ApiPortal isMobile={isMobile} />
     )} />
-    <Route exact path="/analysis" render={({ location }) => (
-      <Analysis isMobile={isMobile} queryString={location.search} query={qs.parse(location.search)} />
-    )} />
     <Route exact path="/a2fv1" render={() => (
       <Redirect to="/?funders=360G-blf&incomeRange=1%2C100000&addressWithin=20km%2C53.404361%2C-2.979554&causes.id=102"/>
     )} />
@@ -36,3 +33,7 @@ Router.propTypes = {
 }
 
 export { Router }
+
+// <Route exact path="/analysis" render={({ location }) => (
+//   <Analysis isMobile={isMobile} queryString={location.search} query={qs.parse(location.search)} />
+// )} />
