669d080878b65c46bb3d832a61f909ad8dc0f763
dan-kwiat <dan.kwiat@gmail.com>
2019-02-14 08:43:10 +0000

    feat(analytics): send pageview on route change

--- src/Router.js
+++ src/Router.js
@@ -9,24 +9,34 @@ import { ApiPortal } from './components/api-portal'
 import { Analysis } from './components/analysis'
 
 const Router = ({ isMobile }) => (
-  <Switch>
-    <Route exact path="/" render={({ location }) => (
-      <Charities isMobile={isMobile} queryString={location.search} query={qs.parse(location.search)} />
-    )} />
-    <Route path="/charities/:charityId" render={({ match, location }) => (
-      <Charity isMobile={isMobile} charityId={match.params.charityId} view={qs.parse(location.search).view} />
-    )} />
-    <Route exact path="/about" render={() => (
-      <About isMobile={isMobile} />
-    )} />
-    <Route exact path="/api-portal" render={() => (
-      <ApiPortal isMobile={isMobile} />
-    )} />
-    <Route exact path="/a2fv1" render={() => (
-      <Redirect to="/?funders=360G-blf&incomeRange=1%2C100000&addressWithin=20km%2C53.404361%2C-2.979554&causes.id=102"/>
-    )} />
-    <Redirect to="/"/>
-  </Switch>
+  <React.Fragment>
+    <Route path="/" render={({ location }) => {
+      if (typeof window.ga === 'function') {
+        const path = location.pathname + location.search
+        window.ga('gtag_UA_134443576_1.set', 'page', path)
+        window.ga('gtag_UA_134443576_1.send', 'pageview')
+      }
+      return null
+    }} />
+    <Switch>
+      <Route exact path="/" render={({ location }) => (
+        <Charities isMobile={isMobile} queryString={location.search} query={qs.parse(location.search)} />
+      )} />
+      <Route path="/charities/:charityId" render={({ match, location }) => (
+        <Charity isMobile={isMobile} charityId={match.params.charityId} view={qs.parse(location.search).view} />
+      )} />
+      <Route exact path="/about" render={() => (
+        <About isMobile={isMobile} />
+      )} />
+      <Route exact path="/api-portal" render={() => (
+        <ApiPortal isMobile={isMobile} />
+      )} />
+      <Route exact path="/a2fv1" render={() => (
+        <Redirect to="/?funders=360G-blf&incomeRange=1%2C100000&addressWithin=20km%2C53.404361%2C-2.979554&causes.id=102"/>
+      )} />
+      <Redirect to="/"/>
+    </Switch>
+  </React.Fragment>
 )
 Router.propTypes = {
   isMobile: PropTypes.bool,
