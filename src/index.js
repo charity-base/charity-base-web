892ba42eefa1225f56fea2ddf7ac173cdda95482
dan-kwiat <dan.kwiat@gmail.com>
2019-03-13 13:54:36 +0000

    feat(): wrap app in apollo provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 import './index.css'
 import App from './App'
 import { unregister } from './registerServiceWorker'
 // import registerServiceWorker from './registerServiceWorker'
+import { charityBaseApiKey } from './lib/constants'
 
-ReactDOM.render(
+const client = new ApolloClient({
+  uri: 'https://charitybase.uk/api/graphql',
+  headers: {
+    Authorization: `Apikey ${charityBaseApiKey}`,
+  },
+})
+
+render(
   <BrowserRouter>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </BrowserRouter>,
   document.getElementById('root')
 )
