a291c0626acc7a893fd2bf2c70f3d40b3930cf48
dan-kwiat <dan.kwiat@gmail.com>
2019-03-19 14:41:40 +0000

    refactor(map): extract map components into multiple files

--- src/components/charities/charts/map/index.js
+++ src/components/charities/charts/map/index.js
@@ -1,20 +1,17 @@
 import React, { Component, Fragment, createRef } from 'react'
 import PropTypes from 'prop-types'
 import {
-  CircleMarker,
   Map,
   Rectangle,
   TileLayer,
   Marker,
-  Tooltip,
 } from 'react-leaflet'
 import { Query } from 'react-apollo'
-import { List, Modal, Skeleton } from 'antd'
 import geohash from 'ngeohash'
-import numeral from 'numeral'
-import { transparentize } from 'polished'
-import { AGG_GEOHASH_CHARITIES, LIST_CHARITIES } from '../../../../lib/gql'
+import { AGG_GEOHASH_CHARITIES } from '../../../../lib/gql'
 import cluster from './cluster'
+import ClusterMarker from './ClusterMarker'
+import ClusterListModal from './ClusterListModal'
 
 const INITIAL_ZOOM = 5
 const INITIAL_CENTER = [54.91244, -3.05385]
@@ -24,109 +21,6 @@ const geohashToLatLng = hash => {
   const { latitude, longitude } = geohash.decode(hash)
   return [latitude, longitude]
 }
-
-const formatCount = x => numeral(x).format('0a')
-
-const ClusterTooltip = ({ count }) => (
-  <Tooltip>
-    <div>{formatCount(count)} {count === 1 ? 'charity' : 'charities'}</div>
-  </Tooltip>
-)
-
-const strokeDashArray = (radius, dashCount=5) => {
-  const strokeDashSum = (Math.PI*2*radius)/5
-  return `${0.5*strokeDashSum} ${0.5*strokeDashSum}`
-}
-
-const ClusterMarker = ({ count, center, size, onClick }) => {
-  const radius = 20 + 20*size
-  return (
-    <CircleMarker
-      center={center}
-      stroke={count <= POPUP_LIST_MAX_LENGTH}
-      color='#EC407A'
-      dashArray={strokeDashArray(radius)}
-      fillColor={transparentize(0.5*(1 - size), '#EC407A')}
-      fillOpacity={0.6}
-      radius={radius}
-      onClick={onClick}
-    >
-      <ClusterTooltip count={count} />
-    </CircleMarker>
-  )
-}
-
-const boundingBoxIntersection = (listOfBounds) => {
-  return listOfBounds.reduce((agg, x) => ({
-    top: x && x.top < agg.top ? x.top : agg.top,
-    left: x && x.left > agg.left ? x.left : agg.left,
-    bottom: x && x.bottom > agg.bottom ? x.bottom : agg.bottom,
-    right: x && x.right < agg.right ? x.right : agg.right,
-  }), {
-    top: 90,
-    left: -180,
-    bottom: -90,
-    right: 180,
-  })
-}
-
-const ClusterListModal = ({ bounds, count, geohashes, filters, open, onClose }) => (
-  <Modal
-    visible={open}
-    onCancel={onClose}
-    footer={null}
-    maskClosable={true}
-  >
-    <Query
-      query={LIST_CHARITIES}
-      variables={{ filters: {
-        ...filters,
-        geo: {
-          ...filters.geo,
-          geohashes,
-          boundingBox: boundingBoxIntersection([
-            filters.geo ? filters.geo.boundingBox : null,
-            bounds,
-          ]),
-        }
-      } }}
-    >
-      {({ loading, error, data }) => {
-        if (error) return 'oops something went wrong'
-        return (
-          <div
-            style={{
-              maxHeight: '800px',
-              overflowY: 'scroll',
-              padding: '1em',
-              marginTop: '2em',
-              backgroundColor: '#FAFAFA',
-              borderRadius: '0.5em',
-            }}
-          >
-            <List
-              itemLayout="vertical"
-              size="large"
-              dataSource={loading ? [...new Array(count)].map((_, id) => ({ id })) : data.CHC.getCharities.list}
-              renderItem={item => (
-                <List.Item
-                  key={item.id}
-                >
-                  <Skeleton loading={loading} active>
-                    <List.Item.Meta
-                      title={<a href='/'>{item.names ? item.names[0].value : null}</a>}
-                    />
-                    {item.activities ? item.activities.slice(0, 240)+'...' : null}
-                  </Skeleton>
-                </List.Item>
-              )}
-            />
-          </div>
-        )
-      }}
-    </Query>
-  </Modal>
-)
 
 class CharitiesMap extends Component {
   state = {
@@ -142,9 +36,10 @@ class CharitiesMap extends Component {
     selectedCluster: {
       open: false,
       geohashes: [],
-      count: null,
+      count: 0,
     }
   }
+  mapRef = createRef()
   componentDidMount() {
     this.invalidateSize()
   }
@@ -155,7 +50,6 @@ class CharitiesMap extends Component {
       map.leafletElement.invalidateSize()
     }, 10)
   }
-  mapRef = createRef()
   onChange = ({ center, zoom }) => {
     const map = this.mapRef.current
     if (!map) return
@@ -188,7 +82,7 @@ class CharitiesMap extends Component {
     }))
   }
   relSize = (count, min, max) => {
-    return min === max ? 1 : (count - min)/(max - min)
+    return min === max ? 0.5 : (count - min)/(max - min)
   }
   onModalClose = () => {
     this.setState(s => ({
@@ -208,15 +102,15 @@ class CharitiesMap extends Component {
         variables={{ filters, ...bounds }}
       >
         {({ loading, error, data }) => {
-          const buckets = !loading && data && data.CHC ? (
+          if (error) return null
+          const buckets = loading ? [] : (
             data.CHC.getCharities.aggregate.geo.geohash.buckets.map(x => ({
               key: x.key,
               center: geohashToLatLng(x.key),
               count: x.count,
             }))
-          ) : []
+          )
           const clusters = cluster(buckets, bounds, zoom).sort((a, b) => a.count - b.count)
-          if (error) return null
           return (
             <Fragment>
               <Map
@@ -239,6 +133,7 @@ class CharitiesMap extends Component {
                       key={x.key}
                       count={x.count}
                       center={x.center}
+                      highlighted={x.count <= POPUP_LIST_MAX_LENGTH}
                       size={size}
                       onClick={() => this.onMarkerClick(x)}
                     />
@@ -258,8 +153,8 @@ class CharitiesMap extends Component {
               </Map>
               <ClusterListModal
                 bounds={bounds}
-                onClose={this.onModalClose}
                 filters={filters}
+                onClose={this.onModalClose}
                 {...selectedCluster}
               />
             </Fragment>
