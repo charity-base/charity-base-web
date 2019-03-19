9011b7ef21ca299cd7527118b83404f0f3004caa
dan-kwiat <dan.kwiat@gmail.com>
2019-03-19 14:00:10 +0000

    feat(map): replace cluster list popup with modal

--- src/components/charities/charts/index.js
+++ src/components/charities/charts/index.js
@@ -1,4 +1,4 @@
-import React, { Component, createRef } from 'react'
+import React, { Component, Fragment, createRef } from 'react'
 import PropTypes from 'prop-types'
 // import ContainerSizeConsumer from '../../general/ContainerSizeConsumer'
 // import CharitiesMap from './map'
@@ -9,14 +9,14 @@ import {
   TileLayer,
   Marker,
   Tooltip,
-  Popup,
 } from 'react-leaflet'
 import { Query } from 'react-apollo'
-import { AGG_GEOHASH_CHARITIES, LIST_CHARITIES } from '../../../lib/gql'
+import { List, Modal, Skeleton } from 'antd'
 import geohash from 'ngeohash'
 import numeral from 'numeral'
 import { transparentize } from 'polished'
-import { cluster, geoHashesBounds } from '../../../lib/mapHelpers'
+import { AGG_GEOHASH_CHARITIES, LIST_CHARITIES } from '../../../lib/gql'
+import { cluster } from '../../../lib/mapHelpers'
 
 const INITIAL_ZOOM = 5
 const INITIAL_CENTER = [54.91244, -3.05385]
@@ -31,43 +31,8 @@ const formatCount = x => numeral(x).format('0a')
 
 const ClusterTooltip = ({ count }) => (
   <Tooltip>
-    <div>{formatCount(count)} charities</div>
-    {count <= POPUP_LIST_MAX_LENGTH ? (
-      <div>(click to list)</div>
-    ) : null}
+    <div>{formatCount(count)} {count === 1 ? 'charity' : 'charities'}</div>
   </Tooltip>
-)
-
-const ClusterPopup = ({ count, geohashes, filters }) => (
-  count <= POPUP_LIST_MAX_LENGTH ? (
-    <Popup autoPan={false}>
-      <div style={{ width: '200px' }}>
-        <Query
-          query={LIST_CHARITIES}
-          variables={{ filters: {
-            ...filters,
-            geo: {
-              ...filters.geo,
-              boundingBox: geoHashesBounds(geohashes) // todo: merge with pre-existing filter and map bounds too
-            }
-          } }}
-        >
-          {({ loading, error, data }) => {
-            if (loading) return 'loading...'
-            if (error) return 'oops something went wrong'
-            return data.CHC.getCharities.list.map(x => (
-              <div
-                style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}
-                key={x.id}
-              >
-                {x.names[0].value}
-              </div>
-            ))
-          }}
-        </Query>
-      </div>
-    </Popup>
-  ) : null
 )
 
 const strokeDashArray = (radius, dashCount=5) => {
@@ -75,11 +40,10 @@ const strokeDashArray = (radius, dashCount=5) => {
   return `${0.5*strokeDashSum} ${0.5*strokeDashSum}`
 }
 
-const ClusterMarker = ({ key, count, center, filters, geohashes, size, onClick }) => {
+const ClusterMarker = ({ count, center, size, onClick }) => {
   const radius = 20 + 20*size
   return (
     <CircleMarker
-      key={key}
       center={center}
       stroke={count <= POPUP_LIST_MAX_LENGTH}
       color='#EC407A'
@@ -90,10 +54,81 @@ const ClusterMarker = ({ key, count, center, filters, geohashes, size, onClick }
       onClick={onClick}
     >
       <ClusterTooltip count={count} />
-      <ClusterPopup count={count} geohashes={geohashes} filters={filters} />
     </CircleMarker>
   )
 }
+
+const boundingBoxIntersection = (listOfBounds) => {
+  return listOfBounds.reduce((agg, x) => ({
+    top: x && x.top < agg.top ? x.top : agg.top,
+    left: x && x.left > agg.left ? x.left : agg.left,
+    bottom: x && x.bottom > agg.bottom ? x.bottom : agg.bottom,
+    right: x && x.right < agg.right ? x.right : agg.right,
+  }), {
+    top: 90,
+    left: -180,
+    bottom: -90,
+    right: 180,
+  })
+}
+
+const ClusterListModal = ({ bounds, count, geohashes, filters, open, onClose }) => (
+  <Modal
+    visible={open}
+    onCancel={onClose}
+    footer={null}
+    maskClosable={true}
+  >
+    <Query
+      query={LIST_CHARITIES}
+      variables={{ filters: {
+        ...filters,
+        geo: {
+          ...filters.geo,
+          geohashes,
+          boundingBox: boundingBoxIntersection([
+            filters.geo ? filters.geo.boundingBox : null,
+            bounds,
+          ]),
+        }
+      } }}
+    >
+      {({ loading, error, data }) => {
+        if (error) return 'oops something went wrong'
+        return (
+          <div
+            style={{
+              maxHeight: '800px',
+              overflowY: 'scroll',
+              padding: '1em',
+              marginTop: '2em',
+              backgroundColor: '#FAFAFA',
+              borderRadius: '0.5em',
+            }}
+          >
+            <List
+              itemLayout="vertical"
+              size="large"
+              dataSource={loading ? [...new Array(count)].map((_, id) => ({ id })) : data.CHC.getCharities.list}
+              renderItem={item => (
+                <List.Item
+                  key={item.id}
+                >
+                  <Skeleton loading={loading} active>
+                    <List.Item.Meta
+                      title={<a href='/'>{item.names ? item.names[0].value : null}</a>}
+                    />
+                    {item.activities ? item.activities.slice(0, 240)+'...' : null}
+                  </Skeleton>
+                </List.Item>
+              )}
+            />
+          </div>
+        )
+      }}
+    </Query>
+  </Modal>
+)
 
 class CharitiesChart extends Component {
   state = {
@@ -105,6 +140,11 @@ class CharitiesChart extends Component {
       left: -180,
       bottom: -90,
       right: 180,
+    },
+    selectedCluster: {
+      open: false,
+      geohashes: [],
+      count: null,
     }
   }
   componentDidMount() {
@@ -124,6 +164,7 @@ class CharitiesChart extends Component {
     const bounds = map.leafletElement.getBounds()
     this.setState({
       zoom,
+      center,
       bounds: {
         top: bounds.getNorth(),
         left: bounds.getWest(),
@@ -132,8 +173,17 @@ class CharitiesChart extends Component {
       }
     })
   }
-  onMarkerClick = ({ center, count }) => {
-    if (count <= POPUP_LIST_MAX_LENGTH) return
+  onMarkerClick = ({ center, count, geohashes }) => {
+    if (count <= POPUP_LIST_MAX_LENGTH) {
+      this.setState({
+        selectedCluster: {
+          open: true,
+          geohashes,
+          count,
+        },
+      })
+      return
+    }
     this.setState(s => ({
       center,
       zoom: s.zoom + 1,
@@ -142,9 +192,17 @@ class CharitiesChart extends Component {
   relSize = (count, min, max) => {
     return min === max ? 1 : (count - min)/(max - min)
   }
+  onModalClose = () => {
+    this.setState(s => ({
+      selectedCluster: {
+        ...s.selectedCluster, // to prevent re-triggering list query
+        open: false,
+      }
+    }))
+  }
   render() {
     const { filters, hoveredItem } = this.props
-    const { zooming, center, zoom, bounds } = this.state
+    const { zooming, center, zoom, bounds, selectedCluster } = this.state
     const filtersBounds = filters && filters.geo && filters.geo.boundingBox
     return (
       <Query
@@ -162,45 +220,51 @@ class CharitiesChart extends Component {
           const clusters = cluster(buckets, bounds, zoom).sort((a, b) => a.count - b.count)
           if (error) return null
           return (
-            <Map
-              center={center}
-              zoom={zoom}
-              style={{ width : '100%', height: '100%', position: 'relative', opacity: (zooming || loading) ? 0.5 : 1 }}
-              onViewportChanged={this.onChange}
-              onZoomstart={() => this.setState({ zooming: true })}
-              onZoomend={() => this.setState({ zooming: false })}
-              ref={this.mapRef}
-            >
-              <TileLayer
-                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              {zooming ? null : clusters.map(x => {
-                const size = this.relSize(x.count, clusters[0].count, clusters[clusters.length - 1].count)
-                return (
-                  <ClusterMarker
-                    key={x.key}
-                    count={x.count}
-                    center={x.center}
-                    geohashes={x.geohashes}
-                    filters={filters}
-                    size={size}
-                    onClick={() => this.onMarkerClick(x)}
-                  />
-                )
-              })}
-              {filtersBounds ? (
-                <Rectangle bounds={[
-                  [filtersBounds.bottom, filtersBounds.left],
-                  [filtersBounds.top, filtersBounds.right],
-                ]} color="black" />
-              ) : null}
-              {hoveredItem && hoveredItem.latitude && hoveredItem.longitude ? (
-                <Marker
-                  position={[hoveredItem.latitude, hoveredItem.longitude]}
+            <Fragment>
+              <Map
+                center={center}
+                zoom={zoom}
+                style={{ width : '100%', height: '100%', position: 'relative', opacity: (zooming || loading) ? 0.5 : 1 }}
+                onViewportChanged={this.onChange}
+                onZoomstart={() => this.setState({ zooming: true })}
+                onZoomend={() => this.setState({ zooming: false })}
+                ref={this.mapRef}
+              >
+                <TileLayer
+                  attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-              ) : null}
-            </Map>
+                {zooming ? null : clusters.map(x => {
+                  const size = this.relSize(x.count, clusters[0].count, clusters[clusters.length - 1].count)
+                  return (
+                    <ClusterMarker
+                      key={x.key}
+                      count={x.count}
+                      center={x.center}
+                      size={size}
+                      onClick={() => this.onMarkerClick(x)}
+                    />
+                  )
+                })}
+                {filtersBounds ? (
+                  <Rectangle bounds={[
+                    [filtersBounds.bottom, filtersBounds.left],
+                    [filtersBounds.top, filtersBounds.right],
+                  ]} color="black" />
+                ) : null}
+                {hoveredItem && hoveredItem.latitude && hoveredItem.longitude ? (
+                  <Marker
+                    position={[hoveredItem.latitude, hoveredItem.longitude]}
+                  />
+                ) : null}
+              </Map>
+              <ClusterListModal
+                bounds={bounds}
+                onClose={this.onModalClose}
+                filters={filters}
+                {...selectedCluster}
+              />
+            </Fragment>
           )
         }}
       </Query>
