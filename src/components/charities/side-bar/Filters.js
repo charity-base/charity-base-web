89531aa508db8afea5bd3af5dc86929f31deefdc
dan-kwiat <dan.kwiat@gmail.com>
2019-03-26 14:51:01 +0000

    feat(side-bar): add message when no filters applied

--- src/components/charities/side-bar/Filters.js
+++ src/components/charities/side-bar/Filters.js
@@ -1,35 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Tag } from 'antd'
+import { NoneText } from '../../general/NoneText'
 
 const Filters = ({ filtersList, onClose }) => (
   <div style={{ width: '100%', textAlign: 'center' }}>
-    {filtersList.map((x, i) => {
-      const { icon, primary } = x
-      return (
-        <div key={x.id}>
-          <Tag
-            closable
-            afterClose={() => onClose(x)}
-            color='#EC407A'
-            style={{
-              cursor: 'default',
-              borderColor: '#EC407A',
-              marginBottom: '4px',
-              // boxSizing: 'border-box',
-              // overflow: 'hidden',
-              // textOverflow: 'ellipsis',
-            }}
-            title={primary}
-          >
-            {icon}
-            <span style={{ marginLeft: '5px' }}>
-              {primary.length > 20 ? `${primary.slice(0, 20).trim()}...` : primary}
-            </span>
-          </Tag>
-        </div>
-      )
-    })}
+    {filtersList.length > 0 ? (
+      filtersList.map((x, i) => {
+        const { icon, primary } = x
+        return (
+          <div key={x.id}>
+            <Tag
+              closable
+              afterClose={() => onClose(x)}
+              color='#EC407A'
+              style={{
+                cursor: 'default',
+                borderColor: '#EC407A',
+                marginBottom: '4px',
+              }}
+              title={primary}
+            >
+              {icon}
+              <span style={{ marginLeft: '5px' }}>
+                {primary.length > 20 ? `${primary.slice(0, 20).trim()}...` : primary}
+              </span>
+            </Tag>
+          </div>
+        )
+      })
+    ) : (
+      <NoneText>No filters applied</NoneText>
+    )
+  }
   </div>
 )
 Filters.propTypes = {
