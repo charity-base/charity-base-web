b0a88c159cdc685a7ed66f888a8c534d971648ac
dan-kwiat <dan.kwiat@gmail.com>
2019-03-27 12:31:53 +0000

    refactor(auth): move auth handling to App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React, { Component, Fragment } from 'react'
-import { Router } from './Router'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { LocaleProvider } from 'antd'
 import enGB from 'antd/lib/locale-provider/en_GB'
-import AuthHandler from './components/general/AuthHandler'
+import auth from './lib/auth'
+import { Router } from './Router'
 import './App.scss'
 
 class App extends Component {
+  componentDidMount() {
+    auth.handleAuthentication(this.context.router.history)
+    auth.scheduleRenewal()
+  }
   render() {
     const innerWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth || 320
     const isMobile = innerWidth < 992
     return (
       <LocaleProvider locale={enGB}>
-        <Fragment>
-          <Router isMobile={isMobile}/>
-          <AuthHandler />
-        </Fragment>
+        <Router isMobile={isMobile}/>
       </LocaleProvider>
     )
   }
+}
+App.contextTypes = {
+  router: PropTypes.object,
 }
 
 export default App
