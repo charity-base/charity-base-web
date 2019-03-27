da7540c1377ef8dd29a257a94802b5120e0cc618
dan-kwiat <dan.kwiat@gmail.com>
2019-03-27 14:13:19 +0000

    feat(styles): customise side bar button

---
+++ src/components/charities/side-bar/Button.js
@@ -0,0 +1,13 @@
+import { Button } from 'antd'
+import styled from 'styled-components'
+
+const SideBarButton = styled(Button)`
+  background-color: inherit !important;
+  color: inherit !important;
+  width: 100%;
+  &:hover, &:active {
+    border-color: #EC407A !important;
+  }
+`
+
+export default SideBarButton
