d532b30d3840198151d8d5e3f5dea396b5cc2a33
dan-kwiat <dan.kwiat@gmail.com>
2019-04-03 18:32:04 +0100

    feat(playground): replace api key input with select

--- src/components/api-portal/playground/ApiKeyModal.js
+++ src/components/api-portal/playground/ApiKeyModal.js
@@ -1,37 +1,105 @@
-import React, { useState } from 'react'
-import { Modal, Button, Input } from 'antd'
-import { InfoText } from '../../general/InfoText'
+import React, { Fragment, useState } from 'react'
+import PropTypes from 'prop-types'
+import { Modal, Button, Typography, Select } from 'antd'
+import authClient from '../../../lib/authApolloClient'
+import { Query } from 'react-apollo'
+import auth from '../../../lib/auth'
+import { LIST_KEYS } from '../../../lib/gql'
+import { LogIn } from '../../general/LogInOrOut'
 
-const ApiKeyModal = ({ isOpen, onChange, onClose }) => {
-  const [value, setValue] = useState('')
+const { Paragraph } = Typography
+const { Option } = Select
+
+const PromptLogIn = () => {
+  return (
+    <div>
+      <Paragraph>
+        Log In to play
+      </Paragraph>
+      <LogIn />
+    </div>
+  )
+}
+
+const ApiKeyModal = ({ currentKey, isOpen, onChange, onClose }) => {
+  const [apiKey, setApiKey] = useState(currentKey)
   return (
     <Modal
-      title={`API Key`}
       visible={isOpen}
-      onCancel={onClose}
+      onCancel={() => {
+        setApiKey(currentKey)
+        onClose()
+      }}
       footer={null}
       maskClosable={true}
     >
-      <Input
-        size='large'
-        placeholder='API Key'
-        value={value}
-        onChange={e => setValue(e.target.value)}
-      />
-      <InfoText center>
-        Get your API Key from the <a href='/api-portal'>API Portal</a>
-      </InfoText>
-      <Button
-        type='primary'
-        icon='enter'
-        style={{ width: '100%' }}
-        onClick={() => onChange(value)}
-        disabled={!value}
-      >
-        Set API Key
-      </Button>
+      <div style={{
+        padding: '2em',
+      }}>
+        {auth.isAuthenticated() ? (
+          <Query
+            client={authClient}
+            query={LIST_KEYS}
+          >
+            {({ loading, error, data }) => {
+              if (error) return 'error oops'
+              const keys = data && data.apiKeys ? data.apiKeys.listKeys : []
+              if (keys.length === 0) {
+                return (
+                  <Paragraph>
+                    Create an API key to get started
+                  </Paragraph>
+                )
+              }
+              return (
+                <Fragment>
+                  <Select
+                    onChange={x => setApiKey(x)}
+                    placeholder='Select an API key to use'
+                    value={apiKey}
+                    style={{
+                      width: '100%',
+                    }}
+                    size='large'
+                  >
+                    {keys.map(x => (
+                      <Option key={x.id} value={x.id}>{x.id}</Option>
+                    ))}
+                  </Select>
+                  <Paragraph
+                    code
+                    style={{
+                      margin: '1em 0',
+                      visibility: apiKey ? 'visible' : 'hidden',
+                    }}
+                  >
+                    Authorization: Apikey {apiKey}
+                  </Paragraph>
+                  <Button
+                    type='primary'
+                    icon='enter'
+                    style={{ width: '100%' }}
+                    onClick={() => onChange(apiKey)}
+                    disabled={loading || !apiKey}
+                  >
+                    Set Authorization Header
+                  </Button>
+                </Fragment>
+              )
+            }}
+          </Query>
+        ) : (
+          <PromptLogIn />
+        )}
+      </div>
     </Modal>
   )
+}
+ApiKeyModal.propTypes = {
+  currentKey: PropTypes.string,
+  isOpen: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+  onClose: PropTypes.func.isRequired
 }
 
 export default ApiKeyModal
