e7dac67883c02eb1f553bcb372fb68c98a76ee56
dan-kwiat <dan.kwiat@gmail.com>
2019-04-03 11:33:38 +0100

    feat(auth): refresh client if auth token updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
 import { LocaleProvider } from 'antd'
 import enGB from 'antd/lib/locale-provider/en_GB'
+import { charityBaseGqlApiUri, charityBaseApiKey } from './lib/constants'
 import auth from './lib/auth'
 import { Router } from './Router'
 import './App.scss'
 
 class App extends Component {
+  state = {
+    accessToken: auth.accessToken,
+  }
   componentDidMount() {
-    auth.handleAuthentication(this.context.router.history)
-    auth.scheduleRenewal()
+    auth.handleAuthentication(
+      this.context.router.history,
+      this.onAccessTokenChange,
+    )
+  }
+  onAccessTokenChange = accessToken => {
+    this.setState({ accessToken })
   }
   render() {
+    const client = new ApolloClient({
+      uri: charityBaseGqlApiUri,
+      headers: {
+        Authorization: `Apikey ${charityBaseApiKey}, Bearer ${this.state.accessToken || ''}`,
+      },
+    })
     const innerWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth || 320
     const isMobile = innerWidth < 992
     return (
-      <LocaleProvider locale={enGB}>
-        <Router isMobile={isMobile}/>
-      </LocaleProvider>
+      <ApolloProvider client={client}>
+        <LocaleProvider locale={enGB}>
+          <Router isMobile={isMobile}/>
+        </LocaleProvider>
+      </ApolloProvider>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,30 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from 'react-apollo'
-import './index.css'
 import App from './App'
 import { unregister } from './registerServiceWorker'
-// import registerServiceWorker from './registerServiceWorker'
-import { charityBaseGqlApiUri, charityBaseApiKey } from './lib/constants'
-import auth from './lib/auth'
-
-const client = new ApolloClient({
-  uri: charityBaseGqlApiUri,
-  headers: {
-    Authorization: `Apikey ${charityBaseApiKey}, Bearer ${auth.accessToken || ''}`,
-  },
-})
+import './index.css'
 
 render(
   <BrowserRouter>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </BrowserRouter>,
   document.getElementById('root')
 )
 
 unregister()
-// registerServiceWorker()
