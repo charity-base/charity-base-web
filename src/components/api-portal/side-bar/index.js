57225631b838a6421dad14081cddcdd3b1afeb77
dan-kwiat <dan.kwiat@gmail.com>
2019-04-04 16:46:39 +0100

    feat(api-portal): add header link to homepage in side bar

--- src/components/api-portal/side-bar/index.js
+++ src/components/api-portal/side-bar/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { NavLink } from 'react-router-dom'
 import { Icon, Menu, Typography } from 'antd'
+import { HomeLink } from '../../general/Layout'
 
 const { Title } = Typography
 const { Item } = Menu
@@ -9,22 +10,35 @@ const { Item } = Menu
 const SideBarContent = ({ menuItems }) => {
   return (
     <div>
-      <Title level={3} style={{ color: 'inherit', textAlign: 'center', padding: '1em' }}>
-        API Portal
-      </Title>
+      <div
+        style={{
+          padding: '1em',
+          textAlign: 'center',
+        }}
+      >
+        <HomeLink href="/">CharityBase</HomeLink>
+        <Title
+          level={4}
+          style={{
+            color: 'inherit',
+          }}
+        >
+          <Icon type='api' />
+          API Portal
+        </Title>
+      </div>
       <Menu
         selectable={false}
         mode='inline'
         style={{ borderStyle: 'none' }}
         theme='dark'
       >
-        {menuItems.map(({ path, label, icon }) => (
+        {menuItems.map(({ path, label }) => (
           <Item
             key={`path-${path}`}
           >
             {path === 'docs' ? (
               <a href='https://github.com/charity-base/charity-base-api'>
-                <Icon type={icon} />
                 {label}
                 <Icon type='link' style={{ marginLeft: '0.5em' }} />
               </a>
@@ -36,7 +50,6 @@ const SideBarContent = ({ menuItems }) => {
                   color: '#EC407A',
                 }}
               >
-                <Icon type={icon} />
                 {label}
               </NavLink>
             )}
@@ -50,7 +63,6 @@ SideBarContent.propTypes = {
   menuItems: PropTypes.arrayOf(PropTypes.shape({
     path: PropTypes.string.isRequired,
     label: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
   })).isRequired,
 }
 
