24f2ea325ca8fe74433802b16f4141958ebd1daf
dan-kwiat <dan.kwiat@gmail.com>
2021-01-25 22:18:57 +0000

    chore(env): use env vars to store constants

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,23 +1,27 @@
-const domain = 'https://charitybase.uk'
+const domain = "https://charitybase.uk"
 
-const isProduction = process.env.NODE_ENV === 'production'
+const isProduction = process.env.NODE_ENV === "production"
 
-const charityBaseGqlApiUri = isProduction ? `${domain}/api/graphql` : '/api/graphql'
+const charityBaseGqlApiUri = isProduction
+  ? `${domain}/api/graphql`
+  : "/api/graphql"
 
-const charityBaseGqlAuthUri = isProduction ? `${domain}/auth/graphql` : '/auth/graphql'
+const charityBaseGqlAuthUri = isProduction
+  ? `${domain}/auth/graphql`
+  : "/auth/graphql"
 
-const charityBaseApiKey = 'my-charity-base-api-key'
+const charityBaseApiKey = process.env.REACT_APP_CHARITY_BASE_API_KEY
 
 const auth0Config = {
-  audience: 'https://charitybase.uk/api',
-  domain: 'charity-base.eu.auth0.com',
-  clientID: 'my-auth0-client-id',
-  redirectUri: isProduction ? domain : 'http://localhost:3000',
-  responseType: 'token id_token',
-  scope: 'openid profile email edit:apikeys',
+  audience: "https://charitybase.uk/api",
+  domain: "charity-base.eu.auth0.com",
+  clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
+  redirectUri: isProduction ? domain : "http://localhost:3000",
+  responseType: "token id_token",
+  scope: "openid profile email edit:apikeys",
 }
 
-const mapBoxToken = 'my-mapbox-access-token'
+const mapBoxToken = process.env.REACT_APP_MAPBOX_TOKEN
 
 export {
   charityBaseApiKey,
